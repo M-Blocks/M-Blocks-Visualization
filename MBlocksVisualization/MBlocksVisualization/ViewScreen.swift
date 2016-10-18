405aa23820cd8ad1571296837a48ee570b9adf64
mateocorrea <mateo8correa@gmail.com>
2016-10-18 18:04:34 -0400

    Seamless adding and removing of cubes
    Database can add/remove cubes at any time

--- MBlocksVisualization/MBlocksVisualization/ViewScreen.swift
+++ MBlocksVisualization/MBlocksVisualization/ViewScreen.swift
@@ -12,7 +12,7 @@ import SceneKit
 class ViewScreen: UIViewController, HomeModelProtocal {
 
     @IBOutlet weak var scnView: SCNView!
-    //var scnView: SCNView!
+    @IBOutlet weak var BlockNumberLabel: UILabel!
     var scnScene: SCNScene!
     var cameraNode: SCNNode!
     var lastTime: TimeInterval = 0
@@ -90,7 +90,7 @@ class ViewScreen: UIViewController, HomeModelProtocal {
     func setupScene() {
         scnScene = SCNScene()
         scnScene.background.contents = "Resources/Background_Diffuse.png"
-        
+        scnScene.physicsWorld.gravity = SCNVector3(0,0,0)
         scnView.scene = scnScene
     }
 
@@ -107,21 +107,35 @@ class ViewScreen: UIViewController, HomeModelProtocal {
     // Used to delete unnecessary objects
     func cleanScene() {
         for node in scnScene.rootNode.childNodes {
-            if node.presentation.position.y < -2 {
+            if node.name == nil {
+                continue
+            }
+            var exists = false
+            for b in feedItems {
+                let bl = b as! BlockModel
+                if bl.cubeNumber! == node.name! {
+                    exists = true
+                    break
+                }
+            }
+            if !exists {
+                print("\(node.name!) not exists")
+                blockModels.removeValue(forKey: node.name!)
                 node.removeFromParentNode()
             }
         }
+        
     }
     
     // FIX
     // Handles what happens when a block is touched
     func handleTouchFor(node: SCNNode) {
-        
-        
+
         let box = blockModels[node.name!]!
-        
+        box.xPosGoal = String(Int(box.xPos!)! + 2)
         print("You touched: \(box.cubeNumber), x: \(box.xPos), y: \(box.yPos), z: \(box.zPos)")
         sendMyRequest(box)
+        
     }
     
     // Finds out what block is touched and calls the function that deals with it
@@ -150,6 +164,7 @@ class ViewScreen: UIViewController, HomeModelProtocal {
     }
     func itemsDownloaded(_ items: NSArray) {
         feedItems = items
+        BlockNumberLabel.text = "Blocks Avalaible: \(feedItems.count)"
     }
     // Sends updated Block data to the database
     func sendMyRequest(_ block: BlockModel) {
@@ -189,6 +204,7 @@ class ViewScreen: UIViewController, HomeModelProtocal {
         if !baseInitiated {
             setupBase()
         }
+        
         for item in feedItems {
             let b = item as! BlockModel
             let cubeNum = b.cubeNumber!
@@ -197,12 +213,15 @@ class ViewScreen: UIViewController, HomeModelProtocal {
             if oldCube != nil {
                 /* first need to check if that blockModel even exists) */
                 /* PROBABLY NEVER NEEDS RE RENDERING JUST TRANSLATION */
-                if needsReRendering(old: oldCube!, new: b) {
+                /*if needsReRendering(old: oldCube!, new: b) {
                     print("Update old cube")
                     oldCube!.sceneNode?.removeFromParentNode()
                     addBlock(block: b, blockNum: cubeNum)
                 } else {
                     
+                }*/
+                if needsReRendering(old: oldCube!, new: b) { // FIX: actually checking if needs update, not if needs rerendering
+                    updateBlock(old: oldCube!, new: b)
                 }
             } else {
                 print("Add new cube")
@@ -218,14 +237,30 @@ class ViewScreen: UIViewController, HomeModelProtocal {
         var geometry:SCNGeometry
         geometry = SCNBox(width: 20.0, height: 1.0, length: 20.0, chamferRadius: 0.0)
         
-        let color = UIColor.black
+        let color = UIColor.lightGray
         
         geometry.materials.first?.diffuse.contents = color
         
         let geometryNode = SCNNode(geometry: geometry)
         geometryNode.position = SCNVector3(x: 0, y: -1, z: 0)
         
+        geometryNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        
         scnScene.rootNode.addChildNode(geometryNode)
+        
+        
+        /*
+        // new try
+        let myFloor = SCNFloor()
+        let myFloorNode = SCNNode(geometry: myFloor)
+        myFloorNode.position = SCNVector3(x: 0, y: -0.5, z: 0)
+        myFloor.reflectivity = 0.9
+        myFloor.reflectionResolutionScaleFactor = 1.0
+        myFloor.reflectionFalloffStart = 2.0
+        myFloor.reflectionFalloffEnd = 10.0
+        scnScene.rootNode.addChildNode(myFloorNode)
+        //
+         */
         
         baseInitiated = true
     }
@@ -252,25 +287,27 @@ class ViewScreen: UIViewController, HomeModelProtocal {
         geometryNode.name = block.cubeNumber
         
         let sideOne = SCNMaterial()
-        sideOne.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideOne.diffuse.contents = UIColor(hue: 0.1, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideOne.name = "sideOne"
         let sideTwo = SCNMaterial()
-        sideTwo.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideTwo.diffuse.contents = UIColor(hue: 0.3, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideTwo.name = "sideTwo"
         let sideThree = SCNMaterial()
-        sideThree.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideThree.diffuse.contents = UIColor(hue: 0.5, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideThree.name = "sideThree"
         let sideFour = SCNMaterial()
-        sideFour.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideFour.diffuse.contents = UIColor(hue: 0.7, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideFour.name = "sidefour"
         let sideFive = SCNMaterial()
-        sideFive.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideFive.diffuse.contents = UIColor(hue: 0.85, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideFive.name = "sideFive"
         let sideSix = SCNMaterial()
-        sideSix.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        sideSix.diffuse.contents = UIColor(hue: 1.0, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
         sideSix.name = "sideSix"
-        
         geometry.materials = [sideOne, sideTwo, sideThree, sideFour, sideFive, sideSix]
+        
+        geometryNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        
         
         scnScene.rootNode.addChildNode(geometryNode)
         totalRenders = totalRenders+1
@@ -288,6 +325,65 @@ class ViewScreen: UIViewController, HomeModelProtocal {
             }
         }
         return false
+    }
+    
+    func updateBlock(old: BlockModel, new: BlockModel) {
+        let variables = ["xPos", "yPos", "zPos", "xOri", "yOri", "zOri", "color", "xPosGoal", "yPosGoal", "zPosGoal", "xOriGoal", "yOriGoal", "zOriGoal", "colorGoal"]
+        
+       /* for v in variables {
+            if v == "xPos" {
+                if abs(Float(old.xPos!)! - Float(new.xPos!)!) < 0.01 {
+                    old.setValue((new.value(forKey: v) as! String), forKey: v)
+                } else {
+                    let deltaX = 0.2 * (Float(new.xPos!)! - Float(old.xPos!)!)
+                    let newX = Float(old.value(forKey: v) as! String)! + deltaX
+                    old.setValue(String(newX), forKey: "xPos")
+                }
+            } else {
+                old.setValue((new.value(forKey: v) as! String), forKey: v)
+            }
+        }*/
+        for v in variables {
+            if ["xPos", "yPos", "zPos"].contains(v) {
+                
+                if abs(Float((old.value(forKey: v) as! String))! - Float((new.value(forKey: v) as! String))!) < 0.01 {
+                    old.setValue((new.value(forKey: v) as! String), forKey: v)
+                } else {
+                    let deltaX = 0.15 * (Float((new.value(forKey: v) as! String))! - Float((old.value(forKey: v) as! String))!)
+                    let newX = Float(old.value(forKey: v) as! String)! + deltaX
+                    old.setValue(String(newX), forKey: v)
+                }
+            } else {
+                old.setValue((new.value(forKey: v) as! String), forKey: v)
+            }
+        }
+        
+        var hue = CGFloat(0.0)
+        if old.color == "green" {
+            hue = CGFloat(0.4)
+        } else {
+            hue = CGFloat(1.0)
+        }
+        
+        let x = Float(old.xPos!)!
+        let y = Float(old.yPos!)!
+        let z = Float(old.zPos!)!
+        old.sceneNode?.position = SCNVector3(x: x, y: y, z: z)
+        
+        
+        // pivot in negative x direction
+        //pivotTowards(block: old, axis: "x", direction: 1)
+        /*let pq = Double((old.sceneNode?.rotation.w)!)
+        old.sceneNode?.pivot = SCNMatrix4MakeTranslation(-0.5, -0.5, 0)
+        old.sceneNode?.position = SCNVector3(x: x-0.5, y: y-0.5, z: z)
+        old.sceneNode?.rotation = SCNVector4(0,0,1, pq + 90.degreesToRadians)*/
+        //old.sceneNode?.pivot = SCNMatrix4MakeTranslation(0.5, 0.5, 0)
+        //old.sceneNode?.position = SCNVector3(x: x+0.5, y: y+0.5, z: z)
+        // FIX: NEED TO RESET PIVOT
+        
+        /*for x in (old.sceneNode?.geometry?.materials)! {
+            x.diffuse.contents = UIColor(hue: hue, saturation: 0.7, brightness: CGFloat(Float(arc4random_uniform(128))/Float(128.0)), alpha: 1.0)
+        }*/
     }
     
     func checkCamera() {
@@ -315,10 +411,36 @@ class ViewScreen: UIViewController, HomeModelProtocal {
         
     }
     
+    func pivotTowards(block: BlockModel, axis: String, direction: Int) {
+        let pq = Double((block.sceneNode?.rotation.w)!)
+        let dir = Double(direction/abs(direction))
+        var trans = [0.0, 0.0, 0.0]
+        if axis == "x" {
+            trans = [0.5*dir, 0.5*dir, 0.0]
+        } else if axis == "y" {
+            trans = [0.0, 0.5*dir, 0.5*dir]
+        } else {
+            trans = [0.5*dir, 0.0, 0.5*dir]
+        }
+        let x = Float(block.xPos!)!
+        let y = Float(block.yPos!)!
+        let z = Float(block.zPos!)!
+        block.sceneNode?.pivot = SCNMatrix4MakeTranslation(Float(trans[0]), Float(trans[1]), Float(trans[2]))
+        block.sceneNode?.position = SCNVector3(x: x+Float(trans[0]), y: y+Float(trans[1]), z: z)
+        
+        if axis == "x" {
+            block.sceneNode?.rotation = SCNVector4(0,0,1, pq + 45.degreesToRadians)
+        } else if axis == "y" {
+            block.sceneNode?.rotation = SCNVector4(1,0,0, pq + 45.degreesToRadians)
+        } else {
+            block.sceneNode?.rotation = SCNVector4(0,1,0, pq + 45.degreesToRadians)
+        }
+        
+    }
+    
 }
 
 extension ViewScreen: SCNSceneRendererDelegate {
-    // 2
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
         checkCamera()
         reRender()
